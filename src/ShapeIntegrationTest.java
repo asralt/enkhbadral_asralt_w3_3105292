d1b340e4b81de91f8c1efa94fdf9e6164fcd5ebc
aska <enkhbadralasral00@gmail.com>
2025-03-19 16:58:38 +0000

    Integration Test class is done

--- src/ShapeIntegrationTest.java
+++ src/ShapeIntegrationTest.java
@@ -8,12 +8,12 @@ public class ShapeIntegrationTest {
     public void testShapesArrayList() {
         ArrayList<Shape> shapes = new ArrayList<>();
 
-        shapes.add(new Circle("Circle1", 3.5));
-        shapes.add(new Circle("Circle2", 4.2));
-        shapes.add(new Rhombus("Rhombus1", 5.0, 6.0, 4.0));
-        shapes.add(new Rhombus("Rhombus2", 7.0, 8.0, 5.0));
-        shapes.add(new RightAngledTriangle("Triangle1", 3.0, 4.0));
-        shapes.add(new RightAngledTriangle("Triangle2", 5.0, 12.0));
+        shapes.add(new Circle("cir1", 3.5));
+        shapes.add(new Circle("cir2", 4.2));
+        shapes.add(new Rhombus("rom11", 5.0, 6.0, 4.0));
+        shapes.add(new Rhombus("rom22", 7.0, 8.0, 5.0));
+        shapes.add(new RightAngledTriangle("triangle1", 3.0, 4.0));
+        shapes.add(new RightAngledTriangle("triangle2", 5.0, 12.0));
 
         for (Shape shape : shapes) {
             assertNotNull(shape.a());
